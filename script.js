eb1c9536034eba44e55778da364fb85a0348f10b
kozmablanka <70952346+kozmablanka@users.noreply.github.com>
2024-09-19 21:09:04 +0000

    csutortok_este

---
+++ script.js
@@ -0,0 +1,56 @@
+const headMen = document.querySelector(".header__menu")
+const buttonOpen = document.querySelector(".header__menu__icon")
+
+
+buttonOpen.addEventListener('click', () => {
+    headMen.classList.toggle('header__open');
+})
+
+
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let seconds = document.getElementById("seconds");
+
+let set_clock = setInterval(() => {
+    let date_now = new Date();
+
+    let hr = date_now.getHours();
+    let min = date_now.getMinutes();
+    let sec = date_now.getSeconds();
+
+    let calc_hr = (hr * 30) + (min / 2);
+    let calc_min = (min * 6) + (sec / 10);
+    let calc_sec = sec * 6;
+
+    hour.style.transform = `rotate(${calc_hr}deg)`;
+    minute.style.transform = `rotate(${calc_min}deg)`;
+    seconds.style.transform = `rotate(${calc_sec}deg)`;
+}, 1000);
+
+setInterval(showTime, 1000);
+ 
+// Defining showTime funcion
+function showTime() {
+    // Getting current time and date
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+ 
+    hour =
+        hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+ 
+    let currentTime =
+        hour +
+        ":" +
+        min +
+        ":" +
+        sec;
+ 
+    // Displaying the time
+    document.getElementById(
+        "digital"
+    ).innerHTML = currentTime;
+}
